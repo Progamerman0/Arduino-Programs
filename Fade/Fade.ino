962d1eaac20c88978c47207d6d1b7c82778177c7
Spencer <spencerkohler52507@gmail.com>
2022-02-28 09:05:23 -0800

    Dual Fade Successful With Port 10

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -17,6 +17,7 @@
 //Date: 2-24-2022
 
 int led = 9;           // the PWM pin the LED is attached to
+int led2 = 10;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
@@ -24,12 +25,27 @@ int fadeAmount = 5;    // how many points to fade the LED by
 void setup() {
   // declare pin 9 to be an output:
   pinMode(led, OUTPUT);
+  // declare pin 10 to be an output:
+  pinMode(led2, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
   analogWrite(led, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(30);
+
+   // set the brightness of pin 10:
+  analogWrite(led2, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
